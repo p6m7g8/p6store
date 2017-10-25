f1fd8d91a17fb5b1b91941cb4676e74f2d966e64
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-10-24 22:40:27 -0400

    checkpoint

--- bin/harness.sh
+++ bin/harness.sh
@@ -2,13 +2,15 @@
 
 main() {
 
-    exit 0
+
     . ../p6test/lib/_colors.sh
     . ../p6test/lib/_util.sh
     . ../p6test/lib/backends/tap.sh
     . ../p6test/lib/asserts/aserts.sh
     . ../p6test/lib/harness.sh
     . ../p6test/lib/api.sh
+
+    exit 0
 
     p6_test_harness_tests_run "t"
 
